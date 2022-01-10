e22b848a65586bdc0d301c5baa57152bd2e1e16f
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-11 00:55:53 +0900

    Completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self.include? '.'
   end
 
   def question?
-
+    self.include? '?'
   end
 
   def exclamation?
-
+    self.include? '!'
   end
 
   def count_sentences
-
+    split_sentence = self.split(/[.?!]/)
+    no_empty_string = split_sentence.reject { |string| string.empty? }
+    no_empty_string.length
   end
 end
